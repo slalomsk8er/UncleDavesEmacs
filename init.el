768295890a762c1267e74190a914cfc5eb3949b2
Dominik Guido Riva <driva@X200s.driva.ch>
2018-04-01 17:28:23 +0200

    Personalisations

--- init.el
+++ init.el
@@ -42,5 +42,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :foundry "1ASC" :family "xos4 Terminus"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :foundry "1ASC" :family "Terminus"))))
  '(fringe ((t (:background "#292b2e")))))
